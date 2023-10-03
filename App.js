720fb8412f9c90799647c55de673200ed4b4204b
alan <2122200433@soy.utj.edu.mx>
2023-10-03 00:49:35 -0600

    nuevos cambios

--- App.js
+++ App.js
@@ -1,61 +1,56 @@
+import { useState } from 'react';
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+import Formulario from './src/componets/formulario.js';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import{StyleSheet,
-  text,
-  View,
-  SafeAreaView,
-  modal,
-  button,
-  Pressable
-}from 'react-native'
-
-
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='inverted' />
+      <Text style={styles.titulo}>Administrador de citas</Text>
+      <Text style={styles.tituloBold}>Veterinaria</Text>
+      <Pressable onPress={() => {setModalVisible(true)}} style={styles.btnNuevaCita}>
+        <Text style={styles.btnTextoNuevaCita}>Nueva Cita</Text>
+      </Pressable>
+      <Formulario modalVisible={modalVisible}/>
+    </SafeAreaView>
   );
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#f3f4f6',
     flex: 1,
-    backgroundColor: '#fff',
-  
+    marginTop: 80,
   },
-
-   titulo:{
-    textAlign:'center',
-    color:'#374151',
-    fontSize:30,
-    fontWeight:'600'
-
-   },
-   tituloBold:{
-    fontWeight:'900',
-    color:'#6d28d9',
-
-   },
-
-   btnNuevaCita:{
-    backgroundColor:'6d28d9',
-    padding:20,
-    margin:20,
-    marginHorizontal:20,
-    borderRadius:10,
-   },
-   btnTextoNuevaCita:{
-    textAlign:'center',
-    color:'afff',
-    fontSize:18,
-    fontWeight:'800',
-    textTransform:'uppercase'
-   }
-                                                                                                                                                               
+  titulo: {
+    marginTop: 30,
+    textAlign: 'center',
+    color: '#374151',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  tituloBold: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#6d28d9',
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6d28d9',
+    padding: 20,
+    marginTop: 20,
+    marginHorizontal: 20,
+    borderRadius: 10,
+  },
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  }
 });
-                                      
--- src/componets/formulario.js
+++ src/componets/formulario.js
@@ -1,69 +1,89 @@
-import React from "react";
-import{
+import React, { useState } from "react";
+import { 
     Modal,
     SafeAreaView,
-    StyleProp,
-    StyleSheet,
-    TextInput,
-    View,
-    text,
-}from 'react-native'
+    StyleSheet, 
+    Text,
+    TextInput, 
+    View
+     } from "react-native";
+import DatePicker from '@dietime/react-native-date-picker';
 
-const formulario = (props) => {
-    const{modalVisible} = props
+function Formulario(props) {
+    const { modalVisible } = props;
+    const [date, setDate] = useState(null);
     return (
-        <Modal>
-        animationType='slide'
-        visible=(props,modalVisible)
-
-        <SafeAreaView>
-            <TexT style={style.titulo}>nuevo{''}
-            <Text style={styleBold}>cita</Text>
-            </Text>
-            <View>
-                <Text>Nombre del pasiente</Text>
-                <TextInput
-               
-                blaceHolder='nombre del pasiente'
-                placeholderTextColor={'#fff'}
-                
-                />
-
-                
-            </View>
-
-        </SafeAreaView>
-        
+        <Modal animationType="slide" visible={modalVisible}>
+            <SafeAreaView style={styles.contenido}>
+                <View ><Text style={styles.tituloChido}>Nueva Cita </Text></View>
+                <View style={styles.campo}>
+                    <Text style={styles.label}>Nombre del paciente: </Text>
+                    <TextInput style={styles.input} placeholder="Nombre del paciente" placeholderTextColor={'#646464'}></TextInput>
+                </View>
+                <View style={styles.campo}>
+                    <Text style={styles.label}>Nombre del propietario: </Text>
+                    <TextInput style={styles.input} placeholder="Nombre del propietario" placeholderTextColor={'#646464'}></TextInput>
+                </View>
+                <View style={styles.campo}>
+                    <Text style={styles.label}>Email del propietario: </Text>
+                    <TextInput style={styles.input} placeholder="Email del propietario" placeholderTextColor={'#646464'} keyboardType="email-address"></TextInput>
+                </View>
+                <View style={styles.campo}>
+                    <Text style={styles.label}>Teléfono del propietario: </Text>
+                    <TextInput style={styles.input} placeholder="Teléfono del propietario" placeholderTextColor={'#646464'} keyboardType="number-pad"></TextInput>
+                </View>
+                <View style={styles.campo}>
+                    <Text style={styles.label}>Fecha Alta</Text>
+                    <DatePicker
+                        value={date}
+                    />
+                </View>
+                <View style={styles.campo}>
+                    <Text style={styles.label}>Sintomas: </Text>
+                    <TextInput style={styles.input} placeholder="Sintomas" placeholderTextColor={'#646464'} keyboardType="number-pad"></TextInput>
+                </View>
+            </SafeAreaView>
         </Modal>
-
-
     )
 }
-const style = StyleSheet.create({
-    contenido:{
-        backgroundColor:'#602809',
-        flex:1,
-    },
-    titulo:{
-        fontSize:30,
-        fontWeight:'600',
-        textAlign:30,
-        tintColor:'#FFF'
-    },
-    
-    titiloBold:{
-        fontWeight:'900'
 
-
+const styles = StyleSheet.create({
+    contenido: {
+        backgroundColor: '',
+        flex: 1,
+        backgroundColor: '#5f35B9'
     },
-    campo:{
-        margin:48
-        mar
+    tituloChido: {
+        marginTop: 30,
+        fontSize: 30,
+        // fontWeight: '800',
+        textAlign: "center"
+    },
+    titulo: {
+        fontSize: 30,
+        fontWeight: '600',
+        testAlign: 'center',
+        marginTop: 30,
+        color: '#fff'
+    },
+    campo: {
+        marginTop: 10,
+        marginHorizontal: 30,
+        color: '#000',
+    },
+    label: {
+        color: '#fff',
+        marginBottom: 10,
+        marginTop: 15,
+        fontSize: 20,
+    },
+    input: {
+        height: 50,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        fontSize: 18,
+        textAlign: "center",
     }
+})
 
-
-
-
-
-    }
-)
+export default Formulario
