0f60d975b258be852867a846975439f9abe9f88c
Diego Antonio <2122300432@soy.utj.edu.mx>
2023-10-10 19:53:54 -0600

    app movil

--- src/componets/Pasiente.js
+++ src/componets/Pasiente.js
@@ -3,6 +3,7 @@ import {
     Text,
     View,
     StyleSheet,
+    Pressable,
 
 } from 'react-native'
 
@@ -20,42 +21,84 @@ const Paciente = ({ item }) => {
 
     }
     return (
-        <View style = {style.contenedor}>
-            <Text style = {style.label}>pasiente:</Text>
-            <Text style = {style.texto}>{pasiente}</Text>
-            <Text style = {style.fecha}>{fornatearFecha(fecha)}</Text>
+        <View style={style.contenedor}>
+            <Text style={style.label}>pasiente:</Text>
+            <Text style={style.texto}>{pasiente}</Text>
+            <Text style={style.fecha}>{fornatearFecha(fecha)}</Text>
+
+           
+            <View style={style.contenedorBotones}>
+                <Pressable style={[style.btn, style.btnEditar]}>
+                    <Text style={style.btnTexto}>Editar</Text>
+                </Pressable>
+                  
+                <Pressable style={[style.btn, style.btnEliminar]}>
+                    <Text style={style.btnTexto}>Eliminar</Text>
+                </Pressable>
+            </View>
         </View>
-        
+
+
 
     )
 }
 const style = StyleSheet.create({
-    contenedor:{
+    contenedor: {
 
-        backgroundColor :'#fff',
-        padding:20,
-        borderBlockColor:'#262c2e',
+        backgroundColor: '#fff',
+        padding: 20,
+        borderBlockColor: '#262c2e',
         borderBottomWidth: 1,
     },
     label: {
-        color:'#2785a4',
-        textTransform:"uppercase",
+        color: '#2785a4',
+        textTransform: "uppercase",
+        fontWeight: '700',
+        marginBottom: 10,
+
+    },
+    texto: {
+        color: '#82c2b2',
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 10,
+
+    },
+    fecha: {
+        color: '#2785a4'
+
+    },
+    contenedorBotones:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginTop:20,
+
+    },
+
+    btn:{
+        paddingVertical: 5,
+        paddingHorizontal:20,
+        borderRadius:5
+
+    },
+    btnEditar:{
+        backgroundColor:'#B7DFC4',
+
+
+    },
+    btnEliminar:{
+        backgroundColor:'#F03128'
+
+    },
+    btnTexto:{
+        textTransform:'uppercase',
         fontWeight:'700',
-        marginBottom:10,
+        fontSize:12,
+        color:'#fffff'
 
-    },
-    texto:{
-        color:'##82c2b2',
-        fontSize:20,
-        fontWeight:'700',
-        marginBottom:10,
+    }
 
-    },
-    fecha:{
-        color:'#2785a4'
 
-    },
-  
 
 })
 
